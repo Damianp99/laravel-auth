b1b51e102517138988fe075417531c6185c24101
Damianp99 <damianperez200199@gmail.com>
2022-03-25 16:49:55 +0100

    added routes

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+
+// GRUPPO DI ROTTE
+Route::middleware('auth')
+    ->name('admin.')
+    ->prefix('admin')
+    ->namespace('Admin')
+    ->group(function () {
+        Route::get('/', 'HomeController@index')->name('home');
+        Route::resource('posts', 'PostController');
+    });
+
+
+Route::get('/', function () {
+    return view('guest.home');
+})->name('guest.home');
