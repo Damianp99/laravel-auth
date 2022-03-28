6be407672ea7e838dfe6c30906b09783bad37c79
Damianp99 <damianperez200199@gmail.com>
2022-03-28 17:19:42 +0200

    added index and show pages

---
+++ resources/views/admin/posts/show.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+@section('content')
+<div class="card container">
+    <h1>{{$post->title}}</h1>
+    <p>{{$post->description}}</p>
+    <div class="d-flex align-items-center justify-content-between py-2">
+        <span>{{$post->created_at}}</span>
+        <a href="{{route('admin.posts.index')}}" class="btn btn-sm btn-info"><i class="fa-solid fa-circle-chevron-left"></i></a>
+    </div>
+   
+</div>
+@endsection
