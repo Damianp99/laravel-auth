f0678f24f0ee0f10d0f30d370c1ff846021e6102
Damianp99 <damianperez200199@gmail.com>
2022-03-28 18:30:34 +0200

    added include form

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -4,21 +4,7 @@
     <h1 class="text-center">Carica il tuo post</h1>
 <form action="{{route('admin.posts.store')}}" method="POST">
     @csrf
-    <div class="form-group">
-        <label for="title">Titolo</label>
-        <input type="title" class="form-control" id="title" name="title" placeholder="Password">
-    </div>
-    <div class="form-group">
-        <label for="image">Immagine</label>
-        <input type="url" class="form-control" id="image" name="image" placeholder="Immagine">
-    </div>
-    <div class="form-group">
-        <label for="description">Example textarea</label>
-        <textarea class="form-control" id="description" name="description" rows="3"></textarea>
-    </div>
-    <div class="d-flex justify-content-end align-items-center">
-        <button type="submit" class="btn btn-sm btn-success">Pubblica</button>
-    </div>
+    @include('includes.form')
 </form>
 </section>
 @endsection
