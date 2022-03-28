120fc01382d9cccf9ecb3852d575bded9775c72a
Damianp99 <damianperez200199@gmail.com>
2022-03-28 16:33:37 +0200

    added controller , model and faker

--- database/seeds/PostSeeder.php
+++ database/seeds/PostSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Models\Post;
+use Faker\Generator as Faker;
 
 class PostSeeder extends Seeder
 {
@@ -9,8 +11,14 @@ class PostSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for ($i = 0; $i < 15; $i++) {
+            $post = new Post();
+            $post->title = $faker->text(20);
+            $post->description = $faker->paragraphs(2, true);
+            $post->image = $faker->imageUrl(250, 250);
+            $post->save();
+        }
     }
 }
